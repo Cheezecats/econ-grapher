2c47e7c6b34dccf643cc3fe9f0850ec6cb2c082a
tigerding <zhiyuanding01@gmail.com>
2026-02-27 21:39:19 -0500

    Add dashed line style

--- src/econgrapher/line.py
+++ src/econgrapher/line.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+from typing import Literal
+import numpy as np
 
 from .element import Element
 
@@ -8,16 +10,45 @@ class Line(Element):
     sy: int
     ex: int
     ey: int
+    style: Literal["solid"] | Literal["dashed"]
 
-    def __init__(self, start: tuple[int, int], end: tuple[int, int]) -> None:
+    def __init__(
+        self,
+        start: tuple[int, int],
+        end: tuple[int, int],
+        style: Literal["solid"] | Literal["dashed"] = "solid",
+    ) -> None:
         super().__init__()
         self.sx, self.sy = start
         self.ex, self.ey = end
+        self.style = style
 
     def render(self, turtle: Turtle) -> None:
-        turtle.penup()
-        turtle.goto(self.sx, self.sy)
-        turtle.pensize(4)
-        turtle.pendown()
-        turtle.goto(self.ex, self.ey)
-        turtle.penup()
+        if self.style == "solid":
+            turtle.penup()
+            turtle.goto(self.sx, self.sy)
+            turtle.pensize(4)
+            turtle.pendown()
+            turtle.goto(self.ex, self.ey)
+            turtle.penup()
+        else:  # dashed
+            dx = self.ex - self.sx
+            dy = self.ey - self.sy
+            total_length = np.sqrt(dx**2 + dy**2)
+            theta = np.arctan2(dy, dx)
+
+            increments = np.arange(0, total_length + 1, 12)
+
+            for incr in increments[::2]:
+                x1 = self.sx + incr * np.cos(theta)
+                y1 = self.sy + incr * np.sin(theta)
+
+                x2 = self.sx + (incr + 10) * np.cos(theta)
+                y2 = self.sy + (incr + 10) * np.sin(theta)
+
+                turtle.penup()
+                turtle.goto(x1, y1)
+                turtle.pensize(4)
+                turtle.pendown()
+                turtle.goto(x2, y2)
+                turtle.penup()
